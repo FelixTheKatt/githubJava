b017bdd63ef78ee09409ff31fd551eee59593dd6
FelixTheKatt <32057142+FelixTheKatt@users.noreply.github.com>
2020-07-08 14:45:20 +0200

    habitant louer service

--- src/Habitant.java
+++ src/Habitant.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 
 public
@@ -6,7 +5,19 @@ class Habitant {
     private String nom;
     private String prenom;
     private Double argent;
-    private ArrayList<Bien> biens ;
+    private ArrayList<Bien> biens;
+
+    public
+    Habitant() {
+    }
+
+    public
+    Habitant(String nom, String prenom, Double argent) {
+        setNom(nom);
+        setPrenom(prenom);
+        setArgent(argent);
+        setBiens(new ArrayList<>());
+    }
 
     public
     String getNom() {
@@ -49,99 +60,103 @@ class Habitant {
     }
 
     public
-    Habitant() {
+    void ajouterBiens(Bien bien) {
+        biens.add(bien);
     }
 
     public
-    Habitant(String nom, String prenom, Double argent) {
-        setNom(nom);
-        setPrenom(prenom);
-        setArgent(argent);
-        setBiens(new ArrayList<>());
-    }
-    public void ajouterBiens (Bien bien){
-        biens.add(bien);
-    }
-    public void ajouterBiens (Bien bien,Bien bien2){
+    void ajouterBiens(Bien bien, Bien bien2) {
         biens.add(bien);
         biens.add(bien2);
     }
-    public void ajouterBiens (Bien bien,Bien bien2,Bien bien3){
+
+    public
+    void ajouterBiens(Bien bien, Bien bien2, Bien bien3) {
         biens.add(bien);
         biens.add(bien2);
         biens.add(bien3);
     }
-    public void ajouterBiens (Bien bien,Bien bien2,Bien bien3,Bien bien4){
+
+    public
+    void ajouterBiens(Bien bien, Bien bien2, Bien bien3, Bien bien4) {
         biens.add(bien);
         biens.add(bien2);
         biens.add(bien3);
         biens.add(bien4);
     }
-    public void ajouterBiens (Bien bien,Bien bien2,Bien bien3,Bien bien4,Bien bien5){
+
+    public
+    void ajouterBiens(Bien bien, Bien bien2, Bien bien3, Bien bien4, Bien bien5) {
         biens.add(bien);
         biens.add(bien2);
         biens.add(bien3);
         biens.add(bien4);
         biens.add(bien5);
     }
-    public void afficherBien(){
+
+    public
+    void afficherBien() {
         getBiens().forEach(x -> System.out.println(x.toString()));
     }
-    public void totalBienValeur(){
+
+    public
+    void totalBienValeur() {
         System.out.println(getBiens().stream().mapToDouble(Bien::getPrix).sum());
     }
-    public void achatBien( Habitant habitant,  Bien bien){
-        habitant.setArgent(habitant.getArgent()+bien.getPrix());
-        setArgent(getArgent()-bien.getPrix());
+
+    public
+    void achatBien(Habitant habitant, Bien bien) {
+        habitant.setArgent(habitant.getArgent() + bien.getPrix());
+        setArgent(getArgent() - bien.getPrix());
         getBiens().add(bien);
         habitant.getBiens().remove(bien);
     }
 
-    public void louerBien ( Habitant habitant, Bien bien, int jour){
-        if (habitant.getBiens().contains(bien)){
-            if (bien instanceof Bijou ){
+    public
+    void louerBien(Habitant habitant, Bien bien, int jour) {
+        if (habitant.getBiens().contains(bien)) {
+            if (bien instanceof Bijou) {
 
-                double prix = (bien.getPrix()*jour);
+                double prix = (bien.getPrix() * jour);
                 double pourcent = (prix * 15) / 100;
-                setArgent(getArgent()-pourcent);
+                setArgent(getArgent() - pourcent);
             }
 
-            if (bien instanceof Livre ){
-                int nb = jour/15;
-                if(jour%15!=0 ||nb==0){
+            if (bien instanceof Livre) {
+                int nb = jour / 15;
+                if (jour % 15 != 0 || nb == 0) {
 
-                    nb+=1;
+                    nb += 1;
                 }
-                double prix = (bien.getPrix()*nb);
+                double prix = (bien.getPrix() * nb);
                 double pourcent = (prix * 10) / 100;
-                setArgent(getArgent()-pourcent);
+                setArgent(getArgent() - pourcent);
             }
-        }
-        else {
+        } else {
             System.out.println("wrong param");
         }
 
     }
 
-    public void louerService(Habitant habitant,String service, int jour){
+    public
+    void louerService(Habitant habitant, String service, int jour) {
         int pricePerDay = 75;
-        int total = jour*pricePerDay;
-        switch (service.toLowerCase()){
+        int total = jour * pricePerDay;
+        switch (service.toLowerCase()) {
             case "tondre":
-                System.out.println(habitant.getNom()+ " tond"+" chez"+" "+getNom()+" "+ jour +" jours"+ total);
-                setArgent(getArgent()+total);
-                habitant.setArgent(getArgent()+total);
+                System.out.println(habitant.getNom() + " tond" + " chez" + " " + getNom() + " " + jour + " jours" + total);
+                setArgent(getArgent() + total);
+                habitant.setArgent(getArgent() + total);
                 break;
             case "laver":
-                System.out.println(habitant.getNom()+ " lave"+" chez"+" "+getNom()+" "+ jour +" "+ total);
-                setArgent(getArgent()+total);
-                habitant.setArgent(getArgent()+total);
+                System.out.println(habitant.getNom() + " lave" + " chez" + " " + getNom() + " " + jour + " " + total);
+                setArgent(getArgent() + total);
+                habitant.setArgent(getArgent() + total);
                 break;
             default:
                 System.out.println("pas de service " + habitant.getNom() + " n'es past un esclave");
         }
     }
-
 
 
     @Override
